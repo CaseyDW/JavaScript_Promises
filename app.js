d27a5bc3d36e5ac02fc2a9cc897ccdb5d46fe9d9
Casey Ward <hitchkra@hotmail.com>
2023-04-18 18:51:46 -0400

    JS Promises Complete

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-/**
- *
- * @returns A promise that is designed to resolve with a list of hobbits, or potentially fail with an failure object. The failure object includes a boolean success property and a string message property.
- */
+"use strict";
+console.log("hello Promises!");
+
 function getList() {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
@@ -15,10 +14,42 @@ function getList() {
   });
 }
 
+
+let feedbackP = document.querySelector("#error");
+let ul = document.querySelector("#list");
+
 // TODO: Handle the resolved or rejected states of the promise
+
+let promise = getList(); 
+
 
 // TODO: If the promise resolves with the list of hobbits
 // Render the list of hobbits as list items within the unordered list with id="list" (check the index.html file)
+function handleList(list) {
+  list.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+}
 
+function handleError(err){
+  console.error(err);
+  feedbackP.textContent = err.message;
+}
 // TODO: If the promise rejects with the failure object
 // Display the failure message in the paragraph element with id="error" (check index.html file)
+async function updateDomList() {
+  try {
+  let list = await getList();
+  list.forEach((hobbit) => {
+    let li = document.createElement("li");
+    li.textContent = hobbit;
+    ul.appendChild(li);
+  });
+} catch (error) {console.error(err);
+  feedbackP.textContent = err.message;
+}
+}
+
+updateDomList();
